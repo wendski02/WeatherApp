1778ad7741f293e7e6aa25ae7d2926c159c95f41
unknown <hartantodasuki026@student.unsrat.ac.id>
2023-09-20 23:55:55 +0800

    Penambahan dan perubahan terakhir

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
 
 const API_URL = environment.API_URL;
 const API_KEY = environment.API_KEY;
@@ -12,13 +13,29 @@ const API_KEY = environment.API_KEY;
 })
 export class HomePage {
 
-  constructor(public httpClient:HttpClient) {
+  weatherTemp: any
+  weatherIcon: any
+  weatherDetails: any
+  cityName = ""
+  name = ""
+  todayDate = new Date()
+  loading = true
+  defaultCity = 'Manado';
+
+  constructor(public httpClient: HttpClient){
     this.loadData()
   }
-
+  
   loadData(){
-    this.httpClient.get(`${API_URL}/weather?q=${"Manado"}&appid=${API_KEY}`).subscribe(results => {
+    this.httpClient.get(`${API_URL}/weather?q=${this.cityName}&appid=${API_KEY}`).subscribe(results => {
       console.log(results);
+      this.weatherTemp = results['main']
+      this.name = results['name']
+      this.weatherDetails = results['weather'][0]
+      this.weatherIcon = `https://openweathermap.org/img/wn/${this.weatherDetails.icon}@4x.png`
+      this.loading = false
+      console.log(this.weatherTemp);
+      console.log(this.weatherDetails);
     })
   }
 
